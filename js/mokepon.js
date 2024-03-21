94a9bed34cf7e21a3bd14e184c118ada699d1a7d
kennedyTEC <90941829+KennedyTEC@users.noreply.github.com>
2024-03-21 09:01:27 -0500

    programando el boton seleccionar

---
+++ js/mokepon.js
@@ -0,0 +1,17 @@
+/*
+PROGRAMANDO EL BOTON SELECCIONAR
+- obtengo la lista de los inputs
+- recorro la lista y corroboro que haya un input seleccionado
+- de haber uno, imprimo su value, de lo contrario, se muestra 'Selecciona una mascota'
+*/
+let botonMascotaJugador = document.getElementById('boton-mascota');
+botonMascotaJugador.addEventListener('click',()=>{
+  let listaMascotas = document.getElementsByName('mascota');
+  let nombreMascota = "Selecciona una mascota";
+  for (let i = 0; i < listaMascotas.length; i++) {
+    if (listaMascotas[i].checked == true) {
+      nombreMascota = listaMascotas[i].value
+    }
+  }
+  alert(nombreMascota)
+})
